d7abdcb1acbee44fc8e4e5ca5716f339da9a5fa6
Aencio <aenpvp9@gmail.com>
2025-02-06 09:16:33 -0300

    daily commit

--- aula4/index.js
+++ aula4/index.js
@@ -1,4 +1,21 @@
-const fs = requrie('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-fs.readdir()
+fs.readdir(path.resolve(__dirname))
+.then(files => console.log(files))
+.catch(e => console.log(e));
+
+
+async function readdir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname)
+    const files = await fs.readdir(rootDir)
+    walk(files)
+}
+
+function walk(files) {
+    for (let file of files) {
+        console.log(file)
+    }
+}
+
+readdir('c:\\Users\\aenpv\\OneDrive\\Documentos\\Nodejs\\ProjetoAgenda\\')
