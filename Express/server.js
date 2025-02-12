32d7a0859c094c6861a1e9d85410a4f9965fa445
Aencio <aenpvp9@gmail.com>
2025-02-12 08:47:01 -0300

    add: mongoStore, session and flash

--- Express/server.js
+++ Express/server.js
@@ -1,4 +1,5 @@
-require('dotnev').config();
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -6,22 +7,42 @@ mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifie
     .then(() => {
         console.log('Conectei a base de dados.');
         app.emit('pronto')
-    });
+    })
+    .catch(e => console.log(e));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 
 const routes = require('./routes');
 const path = require('path');
 const meuMiddleware = require('./src/middlewares/middleware');
 
-app.use(
-    express.urlencoded(
-        {
-          extended: true
-        }
-    )
-);
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*const sessionOptions = session({
+    scret: 'secreto',
+    store: MongoStore.create({mongoUrl: process.env.CONNECTIONSTRING}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+    }
+});
+*/
+app.use(session({
+    scret: 'secreto',
+    store: MongoStore.create({mongoUrl: process.env.CONNECTIONSTRING}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+    }
+}));
+app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));    
 app.set('view engine', 'ejs');
 
